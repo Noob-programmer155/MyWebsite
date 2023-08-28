f545cccd66ed4fdeef30d5b746fa46cf1a91a7aa
amar <rijal.amar29@gmail.com>
2023-08-28 11:02:26 +0700

    adding more letter

--- src/component/lettereditor.js
+++ src/component/lettereditor.js
@@ -8,30 +8,45 @@ export default function ApplicationLetter() {
     document.title = language === "Indonesian"? "Surat Lamaran":"Application Letter"
     Font.registerHyphenationCallback(word => [word])
     return(
-        <PDFViewer width='100%' height={`${window.innerHeight}px`}>
+        <PDFViewer width='100%' height={`${window.innerHeight}px`} style={{border:'none'}}>
             <Document 
-                title={language === "Indonesian"? "Surat Lamaran":"Application Letter"}
-                author="Arrijal Amar Ma`ruf"
-                keywords="Application Letter"
+                // title={language === "Indonesian"? "Surat Lamaran":"Application Letter"}
+                // author="Arrijal Amar Ma`ruf"
+                // keywords="Application Letter"
             >
                 {(language === "Indonesian")?
-                    <GeneralLetterIndonesia 
-                        subject={subjectName}
-                        position={subjectPosition}
-                        address={companyAddress} 
-                        citystatezipcode={companyCitystatezipcode} 
-                        company={companyName} 
-                        job={jobTarget} 
-                        jobSource={jobSource}/>:
-                    <GeneralLetterEnglish 
-                        subject={subjectName}
-                        position={subjectPosition}
-                        gender={subjectGender} 
-                        address={companyAddress}
-                        citystatezipcode={companyCitystatezipcode} 
-                        company={companyName} 
-                        job={jobTarget} 
-                        jobSource={jobSource}/>
+                    <>
+                        {(!subjectName || (subjectName && subjectName.length === 0))?
+                            <GeneralMultiLetterIndonesia
+                                job={jobTarget}
+                            />:
+                            <GeneralLetterIndonesia 
+                                subject={subjectName}
+                                position={subjectPosition}
+                                address={companyAddress} 
+                                citystatezipcode={companyCitystatezipcode} 
+                                company={companyName} 
+                                job={jobTarget} 
+                                jobSource={jobSource}/>
+                        }
+                    </>:
+                    <>
+                        {(!subjectName || (subjectName && subjectName.length === 0))?
+                            <GeneralMultiLetterEnglish
+                                job={jobTarget}
+                            />:
+                            <GeneralLetterEnglish 
+                                subject={subjectName}
+                                position={subjectPosition}
+                                gender={subjectGender} 
+                                address={companyAddress}
+                                citystatezipcode={companyCitystatezipcode} 
+                                company={companyName} 
+                                job={jobTarget} 
+                                jobSource={jobSource}/>
+
+                        }
+                    </>
                 }
             </Document>
         </PDFViewer>
@@ -58,6 +73,20 @@ const styles = StyleSheet.create({
         fontSize: '11pt',
         lineHeight: '1.5pt',
         textIndent: '30px',
+        textAlign:'justify'
+    },
+    section1M: {
+        margin: '40pt 40pt 0 40pt',
+    },
+    section2M: {
+        margin: '30pt 40pt 0 40pt'
+    },
+    descM: {
+        marginTop:'10pt',
+        fontFamily: 'Times-Roman',
+        fontSize: '11pt',
+        lineHeight: '1.5pt',
+        textIndent: '20px',
         textAlign:'justify'
     }
 });
@@ -147,6 +176,98 @@ const GeneralLetterEnglish = (props) => {
                 </Text>
                 <Text style={styles.desc}>
                     {`Thank you for taking the time to review my application. I am excited about the opportunity to work with the skilled team at ${company} and make a valuable contribution to the company's achievements.`}
+                </Text>
+            </View>
+            <View style={{alignItems:'flex-end',...styles.section2,marginTop:'30pt'}}>
+                <Text style={{margin:'20pt 25pt 20pt 0',...styles.general}}>
+                    Sincerely,
+                </Text>
+                <Text style={styles.general}>
+                    Arrijal Amar Ma`ruf
+                </Text>
+            </View>
+        </Page>
+    )
+}
+
+const GeneralMultiLetterIndonesia = (props) => {
+    const {job} = props
+    return(
+        <Page size='A4' style={styles.page}>
+            <View style={styles.section1M} wrap={false}>
+                <Text style={styles.general}>Arrijal Amar Ma`ruf</Text>
+                <Text style={styles.general}>Colomadu, Karanganyar</Text>
+                <Text style={styles.general}>(+62) 089-8868-4379</Text>
+                <Link style={styles.general} src="mailto:amrrrr572@gmail.com">amrrrr572@gmail.com</Link>
+                <Text style={styles.general}>{GenerateDate('ID')}</Text>
+            </View>
+            <View style={styles.section2M}>
+                <Text style={styles.descM}>
+                    {`Saya menulis ini untuk menyatakan minat saya yang kuat dalam mengejar karir yang menantang dan bermanfaat di ${job}. Saya ingin menyumbangkan keterampilan, pengetahuan, dan antusiasme saya pada perusahaan/organisasi dinamis yang menghargai inovasi dan pertumbuhan.`}
+                </Text>
+                <Text style={styles.descM}>
+                    {`Sebagai lulusan baru dengan gelar S1 Teknik Informatika dari Universitas Muhammadiyah Surakarta, saya telah mengembangkan dasar yang kuat dalam Software Development area. Prestasi akademis saya, dipadukan dengan dedikasi saya untuk terus belajar, telah mempersiapkan saya untuk karier yang sukses di ${job}.`}
+                </Text>
+                <Text style={styles.descM}>
+                    Selama perjalanan akademis saya, saya mendapat kesempatan untuk mengikuti bootcamp di Bangkit Academy pada alur pembelajaran Cloud Engineer. Pengalaman ini membekali saya dengan keterampilan praktis dalam berbagai teknologi pemrograman seperti Javascript, HTML/CSS, MySQL, NodeJS serta kemampuan khusus di bidang Cloud Engineer di Google Cloud seperti Iaas (Compute Engine), Paas (App Engine, Cloud Run), and Saas (Google Translate API, Google Vision API), Containerized Machine (Docker,Kubernetes), CI/CD menggunakan Jenkins dan Spinnaker. Selain itu, saya juga memperkuat kemampuan Algoritma dan Struktur data serta kemampuan komunikasi saya, baik itu di Bangkit maupun melalui pembelajaran mandiri. Pengalaman saya di Bangkit ditambah dengan pembelajaran mandiri telah membekali saya untuk dapat merancang, mengembangkan, serta mengatasi masalah pada perangkat lunak  yang ingin saya terapkan dalam lingkungan profesional.
+                </Text>
+                <Text style={styles.descM}>
+                    Saya tertarik pada perusahaan/organisasi yang dikenal karena komitmennya terhadap keunggulan dan dedikasinya dalam mengembangkan bakat pada bidang teknologi. Saya yakin bahwa kemampuan saya untuk beradaptasi, belajar dengan cepat, dan bekerja secara kolaboratif akan menjadikan saya aset bagi tim mana pun.
+                </Text>
+                <Text style={styles.descM}>
+                    Terlampir disini adalah resume saya, yang memberikan rincian lebih lanjut tentang latar belakang pendidikan, keterampilan, serta portfolio saya. Saat saya sedang menjajaki peluang, saya sangat tertarik pada peran yang melibatkan bidang pengembangan perangkat teknologi dan penyelesaian masalah teknologi serta menawarkan peluang untuk berkembang.
+                </Text>
+                <Text style={styles.descM}>
+                    {`Saya menantikan kemungkinan untuk berkontribusi pada perusahaan yang berpikiran maju dan membawa hasrat saya terhadap pengembangan perangkat teknologi terkhusus pada ${job} ke tim baru. Jangan ragu untuk menghubungi saya di (+62) 089-8868-4379 atau `}<Link style={styles.general} src="mailto:amrrrr572@gmail.com">amrrrr572@gmail.com</Link> untuk mendiskusikan bagaimana kualifikasi saya selaras dengan kebutuhan Anda.
+                </Text>
+                <Text style={styles.descM}>
+                    Terima kasih telah mempertimbangkan lamaran saya. Saya gembira dengan potensi untuk berkontribusi terhadap kesuksesan dan pertumbuhan organisasi yang dinamis.
+                </Text>
+            </View>
+            <View style={{alignItems:'flex-end',...styles.section2,marginTop:'30pt'}}>
+                <Text style={{margin:'20pt 15pt 20pt 0',...styles.general}}>
+                    Hormat Saya,
+                </Text>
+                <Text style={styles.general}>
+                    Arrijal Amar Ma`ruf
+                </Text>
+            </View>
+        </Page>
+    )
+}
+
+const GeneralMultiLetterEnglish = (props) => {
+    const {job} = props
+    return(
+        <Page size='A4' style={styles.page}>
+            <View style={styles.section1M} wrap={false}>
+                <Text style={styles.general}>Arrijal Amar Ma`ruf</Text>
+                <Text style={styles.general}>Colomadu, Karanganyar</Text>
+                <Text style={styles.general}>(+62) 089-8868-4379</Text>
+                <Link style={styles.general} src="mailto:amrrrr572@gmail.com">amrrrr572@gmail.com</Link>
+                <Text style={styles.general}>{GenerateDate('ID')}</Text>
+            </View>
+            <View style={styles.section2M}>
+                <Text style={styles.descM}>
+                    {`I'm writing this to express my strong interest in pursuing a challenging and rewarding career at ${job}. I want to contribute my skills, knowledge, and enthusiasm to a dynamic company/organization that values ​​innovation and growth.`}
+                </Text>
+                <Text style={styles.descM}>
+                    {`I am proud to announce that I have successfully completed my Bachelor's Degree in Informatics Engineering from Muhammadiyah University of Surakarta. My unyielding commitment to continuous learning and academic achievements has laid a strong foundation for my expertise in software development. Armed with these skills and knowledge, I am ready to pursue a gratifying career in the ${job} industry.`}
+                </Text>
+                <Text style={styles.descM}>
+                    {`During my academic journey, I had the incredible opportunity to attend a bootcamp at Bangkit Academy that specifically focused on the Cloud Engineer learning path. This immersive experience allowed me to acquire practical skills in various programming technologies, including Javascript, HTML/CSS, MySQL, NodeJS, as well as specific skills in the field of Cloud Engineering at Google Cloud, such as Iaas (Compute Engine), Paas (App Engine, Cloud Run), and Saas (Google Translate API, Google Vision API), Containerized Machine (Docker, Kubernetes), CI/CD using Jenkins and Spinnaker. With my honed Algorithm and Data Structure skills, as well as my communication skills, both at Bangkit and through independent learning, I am confident in my ability to design, develop, and troubleshoot software suitable for professional environments. The knowledge I gained at Bangkit, combined with my self-learning, has prepared me to excel in my career as a ${job}.`}
+                </Text>
+                <Text style={styles.descM}>
+                    I am looking for companies/organizations that have a reputation for their dedication to excellence and nurturing of technology talent. I am confident that my adaptability, quick learning, and collaborative skills will be valuable contributions to any team.
+                </Text>
+                <Text style={styles.descM}>
+                    Enclosed is my resume, which provides further details about my educational background, skills, and portfolio. While I am currently exploring opportunities, I am particularly interested in roles that involve the development of technology toolset and technology problem solving and offer opportunities for growth.
+                </Text>
+                <Text style={styles.descM}>
+                    {`I am excited about the opportunity to join a progressive company and utilize my expertise in developing technology tools for the ${job} position. Please don't hesitate to reach out to me at (+62) 089-8868-4379 or via `}<Link style={styles.general} src="mailto:amrrrr572@gmail.com">amrrrr572@gmail.com</Link> to discuss how my qualifications can meet your requirements.
+                </Text>
+                <Text style={styles.descM}>
+                    Thank you for taking the time to review my application. I am eager to contribute my skills and knowledge to a thriving organization and be a part of its growth and success.
                 </Text>
             </View>
             <View style={{alignItems:'flex-end',...styles.section2,marginTop:'30pt'}}>
